f23e949e76da2513b59ab221be1080fcf496852c
Mark Topper <mark@ulties.com>
2017-06-14 11:26:34 +0200

    Create 2017_06_14_120000_create_some_table.php

--- migrations/2017_06_14_120000_create_some_table.php
+++ migrations/2017_06_14_120000_create_some_table.php
@@ -13,7 +13,7 @@ class CreateSomeTable extends Migration
      */
     public function up()
     {
-        Schema::create('some', function (Blueprint $table) {
+        Schema::create('tableName', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateSomeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('some');
+        Schema::dropIfExists('tableName');
     }
 }
